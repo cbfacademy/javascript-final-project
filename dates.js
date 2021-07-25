9308833608b8fe8f4aa89b700af39cd4016f07b7
Genyus <genyus@gmail.com>
2021-07-26 00:15:58 +0100

    fix: resolve functional errors Updates data file to be loaded correctly

---
+++ dates.js
@@ -0,0 +1,46 @@
+let dates = [
+	{
+		"date": "2600 BC",
+		"title": "Yeast",
+		"caption": "The Egyptians were also pioneers in baking as the first recorded civilization to use yeast in their bread as long ago as 2600 BC.",
+		"image": ""
+	},
+	{
+		"date": "1069",
+		"title": "Sugar",
+		"caption": "Sugar was a luxury in Europe until the early 19th century. The first sugar was recorded in England in 1069.",
+		"image": ""
+	},
+	{
+		"date": "1494",
+		"title": "Icing Sugar",
+		"caption": "Icing sugar was invented. It was originally used as a topping for marchpanes, an almond and sugar dessert.",
+		"image": ""
+	},
+	{
+		"date": "1700s",
+		"title": "Soufflé",
+		"caption": "Soufflé appears in France. Cakes and pastries also begin to appear, thanks to the increasing availability of sugar and the rising of the chef profession",
+		"image": ""
+	},
+	{
+		"date": "1779",
+		"title": "Flour",
+		"caption": "Although the earliest archaeological evidence for wheat seeds crushed between simple millstones to make flour dates to 6000 BC. The Romans were the first to grind seeds on cone mills. In 1779, at the beginning of the Industrial Era, the first steam mill was erected in London.",
+		"image": ""
+	},
+	{
+		"date": "1843",
+		"title": "Baking Powder",
+		"caption": "The first modern version of baking powder was discovered and manufactured by Alfred Bird (1811-1878), British chemist and founder of Bird and Sons Ltd. His improved version of baking powder was created so he could make yeast-free bread for his wife, Elizabeth, who had allergies to eggs and yeast.",
+		"image": ""
+	},
+	{
+		"date": "1856",
+		"title": "Food Colouring",
+		"caption": "William Henry Perkin discovered the first synthetic organic dye, called mauve, used to colour foods, drugs, and cosmetics.",
+		"image": ""
+	}
+];
+
+export { dates };
